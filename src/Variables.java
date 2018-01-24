3c0a8ecd2a91c358ff593222374f6930579e35f1
Michael Balling <mballing@students.mchenry.edu>
2018-01-24 09:12:31 -0600

    Initial commit

---
+++ src/Variables.java
@@ -0,0 +1,22 @@
+public class Variables {
+    public static void main(String[] args){
+        byte byteMark = 127;
+        short miniNum = 32767;
+        int num = 2147483647;
+        long longNum = 9223372036854775807L;
+        float floatNum = 2.71828182845f;
+        double doubleNum = 123.456d;
+        boolean boolVal = false;
+        char firstInitial = 'M';
+
+        System.out.println("**Displaying the values of the variables**");
+        System.out.println("byteMark = " + byteMark);
+        System.out.println("miniNum = " + miniNum);
+        System.out.println("num = " + num);
+        System.out.println("longNum = " + longNum);
+        System.out.println("floatNum = " + floatNum);
+        System.out.println("doubleNum = " + doubleNum);
+        System.out.println("boolVal = " + boolVal);
+        System.out.println("my first intial is " + firstInitial);
+    }
+}
